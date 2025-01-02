ede742643d8b05635fe78f9cceaec7f057b4d2ea
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2025-01-03 02:44:47 +0500

    .

--- src/app/components/WhishlistProduct.tsx
+++ src/app/components/WhishlistProduct.tsx
@@ -31,55 +31,51 @@ const products = [
 
 
 function WhishlistProduct() {
-<<<<<<< HEAD
-
-    
-
-=======
->>>>>>> 666c92a8f5d879e6a98cf004041aa36dd0a93e4b
-  return (
-    <div className='max-w-[66rem] mx-auto py-10'>
-        <div className="products flex items-center justify-evenly flex-wrap">
+    return (
+        <div className='max-w-[66rem] mx-auto py-10'>
+            <div className="products flex items-center justify-evenly flex-wrap">
                 {products.map((card, index) => (
                     <Link href={`/Product/${card.id}`} key={index}>
                         <div className="card w-60 h-[38.5rem]">
-<<<<<<< HEAD
+
                             <div className='bg-[#E8E8E8] h-[28rem]'>
-=======
-                            <div className='bg-[#E8E8E8]'>
->>>>>>> 666c92a8f5d879e6a98cf004041aa36dd0a93e4b
-                                <Image
-                                    src={card.image}
-                                    alt='Products'
-                                    quality={100}
-                                    priority
-<<<<<<< HEAD
-                                    className='w-full h-full'
-=======
->>>>>>> 666c92a8f5d879e6a98cf004041aa36dd0a93e4b
-                                     />
-                            </div>
-                            <div className='content flex justify-center items-center p-6 gap-3 flex-col'>
-                                <div className='text-base font-bold text-[#252B42]'>Graphic Designer</div>
-                                <div className='text-sm text-[#737373]'>English Department</div>
-                                <div className='text-base font-bold flex gap-3'>
-                                    <span className='text-[#BDBDBD]'>$16.58</span>
-                                    <span className='text-[#197B63]'>$6.48</span>
+
+                                <div className='bg-[#E8E8E8]'>
+
+                                    <Image
+                                        src={card.image}
+                                        alt='Products'
+                                        quality={100}
+                                        priority
+
+                                        className='w-full h-full'
+
+
+                                    />
                                 </div>
-                                <div className='flex items-center gap-2 '>
-                                    <div className='w-4 h-4 rounded-full bg-[#23A6F0]'></div>
-                                    <div className='w-4 h-4 rounded-full bg-[#197B63]'></div>
-                                    <div className='w-4 h-4 rounded-full bg-[#E77C40]'></div>
-                                    <div className='w-4 h-4 rounded-full bg-[#252B42]'></div>
+                                <div className='content flex justify-center items-center p-6 gap-3 flex-col'>
+                                    <div className='text-base font-bold text-[#252B42]'>Graphic Designer</div>
+                                    <div className='text-sm text-[#737373]'>English Department</div>
+                                    <div className='text-base font-bold flex gap-3'>
+                                        <span className='text-[#BDBDBD]'>$16.58</span>
+                                        <span className='text-[#197B63]'>$6.48</span>
+                                    </div>
+                                    <div className='flex items-center gap-2 '>
+                                        <div className='w-4 h-4 rounded-full bg-[#23A6F0]'></div>
+                                        <div className='w-4 h-4 rounded-full bg-[#197B63]'></div>
+                                        <div className='w-4 h-4 rounded-full bg-[#E77C40]'></div>
+                                        <div className='w-4 h-4 rounded-full bg-[#252B42]'></div>
+                                    </div>
                                 </div>
                             </div>
+
                         </div>
                     </Link>
 
                 ))}
             </div>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default WhishlistProduct
